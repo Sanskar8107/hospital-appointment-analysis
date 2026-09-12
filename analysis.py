b2e178e445aa781ea2be3d52c95f43d44a69af86
Sanskar8107 <sanskarsul8107@gmail.com>
2026-09-12 22:51:00 +0530

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,22 @@
+# Hospital Appointment Analysis
+
+# About The Project
+This is my first Data Science project as a 1st year MSc Data Science student. 
+The goal of this project is to analyze hospital appointment data to find the busiest doctors and departments.
+
+# Tools Used
+- Python
+- Pandas
+- Matplotlib
+
+# What I Learned
+- How to clean and analyze data using Pandas
+- How to find patterns using value_counts()
+- How to create bar charts for data visualization
+- Basics of GitHub for portfolio building
+
+# Future Improvements
+- Add analysis for appointment cancellation reasons
+- Add time-based analysis (busiest days/hours)
+
+Author: Sanskar - MSc Data Science (1st Year)
